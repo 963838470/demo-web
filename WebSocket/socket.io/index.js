73e6356c7ba5cb58af7530bac1ac6ceefbb70a45
chenbo <963838470@qq.com>
2020-02-29 12:06:05 +0800

    print socket message

--- WebSocket/socket.io/index.js
+++ WebSocket/socket.io/index.js
@@ -6,13 +6,20 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html")
 })
 
+// 监听socket连接
 io.on("connection", function(socket) {
   console.log("a user connected")
+  // 广播，除了消息发送者
+  socket.broadcast.emit("hi")
+  // 监听socket断开连接
   socket.on("disconnect", function() {
     console.log("user disconnected")
   })
+  // 监听自定义事件
   socket.on("chat message", function(msg) {
     console.log("message: " + msg)
+    // 将消息发送给所有人
+    io.emit("chat message", msg)
   })
 })
 
