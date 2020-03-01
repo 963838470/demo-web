2e3c7c2594160fd3fccb3718e265b8894848aef1
chenbo <963838470@qq.com>
2020-03-01 19:11:28 +0800

    socket.io room

--- WebSocket/socket.io/index.js
+++ WebSocket/socket.io/index.js
@@ -19,21 +19,25 @@ io.use((socket, next) => {
   return next(new Error("授权异常"))
 })
 
+const roomid = "some room"
+
 // 监听socket连接
 io.on("connection", function(socket) {
-  let token = socket.handshake.query.token
-  console.log("a user connected:", token)
+  // 加入房间
+  socket.join(roomid)
+  let name = socket.handshake.query.name
+  console.log("a user connected:", socket.id, name)
   // 广播，除了消息发送者
-  socket.broadcast.emit("chat message", "hi")
+  socket.to(roomid).broadcast.emit("chat message", "hi")
   // 监听socket断开连接
   socket.on("disconnect", function() {
-    console.log("user disconnected")
+    console.log("user disconnected", socket.id, name)
   })
   // 监听自定义事件
   socket.on("chat message", function(msg) {
     console.log("message: " + msg)
     // 将消息发送给所有人
-    io.emit("chat message", msg)
+    io.to(roomid).emit("chat message", msg)
   })
 })
 
