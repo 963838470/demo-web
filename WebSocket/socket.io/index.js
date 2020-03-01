91ae7428565748c42117ab4335840a83aca748c4
chenbo <963838470@qq.com>
2020-03-01 21:25:03 +0800

    消息分类

--- WebSocket/socket.io/index.js
+++ WebSocket/socket.io/index.js
@@ -6,10 +6,6 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html")
 })
 
-function isValid() {
-  return true
-}
-
 // middleware
 io.use((socket, next) => {
   let token = socket.handshake.query.token
@@ -22,24 +18,32 @@ io.use((socket, next) => {
 // 监听socket连接
 io.on("connection", function(socket) {
   let name = socket.handshake.query.name
-  let roomid = socket.handshake.query.roomid
+  let roomname = socket.handshake.query.roomname
   // 加入房间
-  socket.join(roomid)
-  console.log("a user connected:", socket.id, name)
-  // 广播，除了消息发送者都可以收到
-  socket.to(roomid).broadcast.emit("用户" + name + "加入房间")
+  socket.join(roomname, () => {
+    console.log(name + "进入房间" + roomname, socket.id)
+    // 广播，除了消息发送者都可以收到
+    socket.to(roomname).broadcast.emit("send", { type: "tip", msg: name + "加入房间" })
+    // 将消息发送给所有人
+    // socket.emit("send", { type: "tip", msg: "当前用户数量:" + socket.client.sockets })
+  })
+
   // 监听掉线设备事件
   socket.on("disconnect", function() {
-    console.log("user disconnected", socket.id, name)
+    console.log(name + "退出房间" + roomname, socket.id)
+    socket.to(roomname).broadcast.emit("send", { type: "tip", msg: name + "退出房间" + roomname })
   })
   // 监听自定义事件
-  socket.on("chat message", function(msg) {
-    console.log("message: " + msg)
-    // 将消息发送给所有人
-    io.to(roomid).emit("chat message", msg)
+  socket.on("send", function(msg) {
+    console.log(name + "发送消息: " + msg)
+    socket.emit("send", { type: "msg", msg: name + "说:" + msg })
   })
 })
 
 http.listen(3000, function() {
   console.log("listening on *:3000")
 })
+
+function isValid() {
+  return true
+}
