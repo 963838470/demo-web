ce5ad5f81751d56749d3b5e7493815c92eb293d2
chenbo <963838470@qq.com>
2020-03-01 23:59:18 +0800

    protoo server

---
+++ WebSocket/protoo/index.js
@@ -0,0 +1,28 @@
+var app = require("express")()
+var http = require("http").createServer(app)
+const protooServer = require("protoo-server")
+
+const options = {
+  maxReceivedFrameSize: 960000, // 960 KBytes.
+  maxReceivedMessageSize: 960000,
+  fragmentOutgoingMessages: true,
+  fragmentationThreshold: 960000
+}
+protooWebSocketServer = new protooServer.WebSocketServer(http, options)
+protooWebSocketServer.on("connectionrequest", async (info, accept, reject) => {
+  const parseUrl = url.parse(info.request.url, true)
+  if (success) {
+    accept("")
+  } else {
+    reject("")
+  }
+})
+
+// index.html
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html")
+})
+
+http.listen(3000, function() {
+  console.log("listening on *:3000")
+})
