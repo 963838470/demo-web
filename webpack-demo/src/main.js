c99a8707965d6e85cab5f91af37ba6f61063e239
chenbo <chenbo@huaweisoft.com>
2021-03-26 15:11:37 +0800

    vue-photo-preview

--- webpack-demo/src/main.js
+++ webpack-demo/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import preview from 'vue-photo-preview'
+import 'vue-photo-preview/dist/skin.css'
 
 Vue.config.productionTip = false
 
+var option = {}
+Vue.use(preview, option)
+
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
---
+++ webpack-demo/src/pages/vue-photo-preview.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>vue-photo-preview</h2>
+    <img src="1.png" preview="0" preview-text="描述文字" />
+    <img src="2.png" preview="0" preview-text="描述文字" />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+div {
+  border: 1px solid lightgray;
+}
+
+img {
+  width: 80px;
+  height: 80px;
+  margin: 10px;
+}
+</style>
